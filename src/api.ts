f72732b76b8b5989c5ea1060b80d198244c4fc9d
Mikaellpc <60710335+mikaellpc4@users.noreply.github.com>
2022-10-17 15:29:58 -0300

    Eslint Setup

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,13 @@
-import express from 'express'
-import admRouter from '@routes/admRoutes'
-import userRouter from '@routes/userRoutes'
+import express from 'express';
+import admRouter from '@routes/admRoutes';
+import userRouter from '@routes/userRoutes';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(userRouter)
-app.use(admRouter)
+app.use(userRouter);
+app.use(admRouter);
 
 app.listen(port, () => {
-    console.log(`API Listening on port ${port}`)
-})
+  console.log(`API Listening on port ${port}`);
+});
