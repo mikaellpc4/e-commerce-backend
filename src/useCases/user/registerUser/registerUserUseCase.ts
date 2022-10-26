82b4f5b8dfa2025f505429fef1b4a601f15243d7
Mikaellpc <60710335+mikaellpc4@users.noreply.github.com>
2022-10-25 22:53:12 -0300

    fix issue when creating a new User entitiy with inexistent props

--- src/useCases/user/registerUser/registerUserUseCase.ts
+++ src/useCases/user/registerUser/registerUserUseCase.ts
@@ -1,6 +1,7 @@
 import User from '@entities/user';
 import IUsersRepository from '@repositories/IUsersRepository';
 import validateRegisterRequest from '@validations/validateRegisterRequest';
+import bcrypt from 'bcrypt';
 import IRegisterUserRequestDTO from './registerUserDTO';
 
 export default class RegisterUserUseCase {
@@ -13,7 +14,28 @@ export default class RegisterUserUseCase {
     data: IRegisterUserRequestDTO,
   ): Promise<void> {
     if (await validateRegisterRequest(next, data)) {
-      const newUser = new User({ ...data, role: 'USER' });
+      const {
+        name,
+        cpf,
+        phone,
+        sex,
+        birthday,
+        email,
+        password,
+      } = data;
+      // Use bcrypt to hash the password
+      // bcrypt.hash is async so a await is necessary
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        name,
+        cpf,
+        phone,
+        sex,
+        birthday,
+        email,
+        password: hashedPassword,
+        role: 'USER',
+      });
       await this.usersRepository.save(newUser);
     }
   }
